3de3411d316db51425edf5f23680212b4906423c
ajinkyadude <ajinkyadude29@gmail.com>
2024-01-29 19:46:30 +0530

    api call initiated

--- App/Module/TopNavigator/Top1.tsx
+++ App/Module/TopNavigator/Top1.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native"
+import {Text, TouchableOpacity, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {changeStartValue} from '../Feed/Store/Reducer';
 
-const Top1 = () => { 
-    return(
-        <View>
-            <Text>Hello Top 1</Text>
-        </View>
-    )
-}
+const Top1 = () => {
+  const dispatch = useDispatch();
+  return (
+    <TouchableOpacity onPress={() => dispatch(changeStartValue(10))}>
+      <Text>Hello Top 1</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default Top1;
